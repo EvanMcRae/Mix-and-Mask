fd9a743511eea927917d79e43a0f52564bf61283
Itsjustrick12 <Rickmjenkins900@gmail.com>
2026-01-31 14:58:14 -0500

    Added Animations For Right Abilites
    Falling and Slide Animations for right Ability UI elements

--- Assets/Scripts/UI/HUD/HealthUI.cs
+++ Assets/Scripts/UI/HUD/HealthUI.cs
@@ -1,12 +1,14 @@
 using Microsoft.Unity.VisualStudio.Editor;
+using System;
 using UnityEngine;
+
 
 public class HealthUI : MonoBehaviour
 {
     //holds mask images
     [SerializeField] Image[] images;
 
-    public void UpdateHealth(int currHealth, int maxHealth)
+    public void UpdateHealth()
     {
         //enable only the necessary amount of images
 
