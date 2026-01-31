c39c0496f12ead2152517e162eb973e3b7cb26b0
EvanMcRae <evanmmcrae@gmail.com>
2026-01-31 18:39:39 -0500

    Fix some more enemy bugs

--- Assets/Scripts/enemyBehavior/enemyBehavior.cs
+++ Assets/Scripts/enemyBehavior/enemyBehavior.cs
@@ -16,9 +16,19 @@ public abstract class EnemyBase : MonoBehaviour
         GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
         if (playerObj != null)
             player = playerObj.transform;
-            
+
         // Find the manager in the scene
         waveManager = Object.FindFirstObjectByType<WaveManager>();
+    }
+
+    public virtual void Update()
+    {
+        if (player == null)
+        {
+            GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+            if (playerObj != null)
+                player = playerObj.transform;
+        }
     }
 
     public virtual void TakeDamage(float dmg)
