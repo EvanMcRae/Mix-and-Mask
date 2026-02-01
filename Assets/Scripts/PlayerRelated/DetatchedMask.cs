d1386dde59b7110285aecd1716ed01383c0136ca
pie <pie@piescode.net>
2026-01-31 20:26:04 -0500

    Added working arrow to jump movement

--- Assets/Scripts/PlayerRelated/DetatchedMask.cs
+++ Assets/Scripts/PlayerRelated/DetatchedMask.cs
@@ -19,10 +19,12 @@ public class DetatchedMask : MonoBehaviour
     [SerializeField] float maxMovementCooldown = 1.5f;
     [SerializeField] float minVelocityToTakeOver = 2.5f;
     [SerializeField] float maxAttachedCooldown = 1f;
+    [SerializeField] RectTransform arrow = null;
     private float movementCooldown = 0;
     private float attachedCooldown = 0;
     private bool isControlling = false;
     private ControllableEnemy.EnemyType controlledEnemyType = ControllableEnemy.EnemyType.None;
+    private bool isDrawing = false; // Checks if the player is actively preparing to launch the mask
 
     public UnityEvent onAttach;
     public UnityEvent onDetach;
@@ -42,6 +44,42 @@ public class DetatchedMask : MonoBehaviour
     {
         if (movementCooldown > 0) movementCooldown -= Time.deltaTime;
         if (attachedCooldown > 0 && !isControlling) attachedCooldown -= Time.deltaTime;
+
+        if (isDrawing)
+        {
+            Vector2 currentMousePos = Mouse.current.position.ReadValue();
+            Vector2 localMousePos;
+            Vector2 maskPosScreenSpace;
+            Vector2 maskPosCanvasSpace;
+
+            maskPosScreenSpace = mainCamera.WorldToScreenPoint(transform.position);
+
+            RectTransform parentRect = arrow.parent as RectTransform;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                parentRect,
+                currentMousePos,
+                null,
+                out localMousePos
+            );
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                parentRect,
+                maskPosScreenSpace,
+                null,
+                out maskPosCanvasSpace
+            );
+            
+            Vector2 arrowDir = localMousePos - maskPosCanvasSpace;
+
+            float angle = Mathf.Atan2(arrowDir.y, arrowDir.x) * Mathf.Rad2Deg;
+
+            float arrowLength = (arrowDir.magnitude / maxRealSlingLength) * maxRealSlingLength;
+            if (arrowLength > maxRealSlingLength) arrowLength = maxRealSlingLength;
+
+            arrow.anchoredPosition = maskPosCanvasSpace;
+            arrow.localRotation = Quaternion.Euler(0, 0, angle);
+            arrow.sizeDelta = new Vector2(arrowLength, 3f);
+        }
     }
 
     public Vector3 GetPositionAtHeight(Vector3 startPos, Vector3 worldDir, float targetHeight)
@@ -66,13 +104,20 @@ public class DetatchedMask : MonoBehaviour
             //Debug.Log("Sling Started");
             initialClickPoint = Mouse.current.position.ReadValue();
             //Debug.Log("Position: " + initialClickPoint.x + ", " + initialClickPoint.y);
+            arrow.gameObject.SetActive(true);
+            isDrawing = true;
         }
-
-        if (movementCooldown > 0) return;
 
         // When released, get mouse position again and launch mask
         if (context.canceled)
         {
+            if (movementCooldown > 0)
+            {
+                arrow.gameObject.SetActive(false);
+                isDrawing = false;
+                return;
+            }
+
             //Debug.Log("Sling released");
             finalClickPoint = Mouse.current.position.ReadValue();
 
@@ -90,6 +135,8 @@ public class DetatchedMask : MonoBehaviour
             _rigidbody.AddForce(new Vector3(wsSlingDirection.x, 0.5f, wsSlingDirection.z) * wsSlingLength, ForceMode.Impulse);
 
             movementCooldown = maxMovementCooldown;
+            arrow.gameObject.SetActive(false);
+            isDrawing = false;
             
             onFling.Invoke(wsSlingDirection);
         }
