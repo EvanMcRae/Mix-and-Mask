25656771a3063faf37e363f78486662535c717f9
EvanMcRae <evanmmcrae@gmail.com>
2026-02-01 16:10:15 -0500

    BUILD

--- Assets/Scripts/Editor/CustomBuildProcessor.cs
+++ Assets/Scripts/Editor/CustomBuildProcessor.cs
@@ -1,13 +1,13 @@
-using UnityEditor.Build;
-using UnityEditor.Build.Reporting;
-using UnityEngine;
+// using UnityEditor.Build;
+// using UnityEditor.Build.Reporting;
+// using UnityEngine;
 
-public class CustomBuildProcessor : IPreprocessBuildWithReport
-{
-    public int callbackOrder => 0;
+// public class CustomBuildProcessor : IPreprocessBuildWithReport
+// {
+//     public int callbackOrder => 0;
 
-    public void OnPreprocessBuild(BuildReport report)
-    {
-        ShaderPassHolder.EnablePass();
-    }
-}
+//     public void OnPreprocessBuild(BuildReport report)
+//     {
+//         ShaderPassHolder.EnablePass();
+//     }
+// }
